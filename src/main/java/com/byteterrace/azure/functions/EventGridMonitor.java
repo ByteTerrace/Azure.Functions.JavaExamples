5558ade97e85219f661b50d9206583a3c1582dde
David Smith <kittoes0124@byteterrace.com>
2021-09-07 16:53:43 -0500

    refuctoring?

--- src/main/java/com/byteterrace/azure/functions/EventGridMonitor.java
+++ src/main/java/com/byteterrace/azure/functions/EventGridMonitor.java
@@ -13,11 +13,14 @@ import com.microsoft.azure.functions.annotation.EventGridTrigger;
 import com.microsoft.azure.functions.ExecutionContext;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
 import java.nio.charset.StandardCharsets;
-import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.logging.Logger;
 import reactor.core.Disposable;
@@ -33,37 +36,124 @@ public class EventGridMonitor {
     private static final Charset Utf8Charset = StandardCharsets.UTF_8;
     private static final TokenCredential TokenCredential = new DefaultAzureCredentialBuilder().build();
 
-    private static Function<Flux<ByteBuffer>, Mono<LineReaderState>> createLineReader(int bufferSize, Charset charset, Consumer<String> lineConsumer) {
-        return (buffer) -> buffer
-            .reduce(new LineReaderState(bufferSize, charset, lineConsumer), LineReaderState.createReducer())
-            .map(state -> {
-                final CharsetDecoder charsetDecoder = state.getCharsetDecoder();
-                final CharBuffer finalDecodedBlock = state.getDecodedBlock();
-                final ByteBuffer finalEncodedBlock = state.getEncodedBlock();
-                final StringBuilder stringBuilder = state.getStringBuilder();
-
-                finalEncodedBlock.flip();
-                charsetDecoder.decode(finalEncodedBlock, finalDecodedBlock, true); // TODO: Handle any potential errors.
-                charsetDecoder.flush(finalDecodedBlock); // TODO: Handle any potential errors.
-                finalDecodedBlock.flip();
-
-                final int l = finalDecodedBlock.limit();
-                final int p = finalDecodedBlock.position();
-
-                stringBuilder.append(finalDecodedBlock.array(), p, (l - p));
-                state.getLineConsumer().accept(stringBuilder.toString());
-                stringBuilder.setLength(0);
-                finalDecodedBlock.position(l);
-
-                return state;
-            });
+    private static String appendFieldChunk(final String fieldValue, final String chunkValue, final int chunkStartIndex, final int chunkLength) {
+        if (0 < chunkLength) {
+            return (fieldValue + chunkValue.substring(chunkStartIndex, (chunkStartIndex + chunkLength)));
+        }
+        else {
+            return fieldValue;
+        }
     }
-    private static int nthIndexOf(final String s, final char c, final int n) {
+    private static void appendFieldValue(final List<String> fieldValues, final String fieldValue, final char escapeChar) {
+        fieldValues.add(((1 < fieldValue.length()) || ((0 < fieldValue.length()) && (fieldValue.charAt(0) != escapeChar)) ? fieldValue : null));
+    }
+    private static Function<Flux<ByteBuffer>, Flux<String>> createLineReader(int bufferSize, Charset charset) {
+        final Mono<LineReaderState> lineReaderState = Mono.just(new LineReaderState(bufferSize, charset));
+
+        return (buffer) -> {
+            return buffer
+                .flatMap(input -> lineReaderState.map(state -> {
+                    final CharsetDecoder charsetDecoder = state.getCharsetDecoder();
+                    final CharBuffer decodedBlock = state.getDecodedBlock();
+                    final ByteBuffer encodedBlock = state.getEncodedBlock();
+                    final ArrayList<String> lines = state.getLines();
+                    final StringBuilder stringBuilder = state.getStringBuilder();
+
+                    boolean isAdditionalDecodingRequired = false;
+                    boolean isNewLineCharacterHandlingEnabled = state.getIsNewLineCharacterHandlingEnabled();
+
+                    encodedBlock.put(input);
+                    encodedBlock.flip();
+                    lines.clear();
+
+                    do {
+                        try {
+                            final CoderResult decodeResult = charsetDecoder.decode(encodedBlock, decodedBlock, false);
+
+                            isAdditionalDecodingRequired = CoderResult.OVERFLOW.equals(decodeResult);
+
+                            if (CoderResult.UNDERFLOW.equals(decodeResult) || isAdditionalDecodingRequired) {
+                                decodedBlock.flip();
+
+                                final char[] a = decodedBlock.array();
+                                final int l = decodedBlock.limit();
+
+                                int o = 0;
+                                int p = decodedBlock.position();
+
+                                if (o < l) {
+                                    do {
+                                        final int c = a[o++];
+
+                                        if ('\r' != c) {
+                                            if (('\n' == c) && isNewLineCharacterHandlingEnabled) {
+                                                stringBuilder.append(a, p, ((o - p) - 1));
+                                                lines.add(stringBuilder.toString());
+                                                stringBuilder.setLength(0);
+                                                p = o;
+                                            }
+
+                                            isNewLineCharacterHandlingEnabled = true;
+                                        }
+                                        else {
+                                            stringBuilder.append(a, p, ((o - p) - 1));
+                                            lines.add(stringBuilder.toString());
+                                            stringBuilder.setLength(0);
+                                            p = o;
+                                            isNewLineCharacterHandlingEnabled = false;
+                                        }
+                                    } while (o < l);
+                                }
+
+                                stringBuilder.append(a, p, (l - p));
+                                decodedBlock.clear();
+                            }
+                            else {
+                                decodeResult.throwException();
+                            }
+                        }
+                        catch (final CharacterCodingException e) {
+                            throw new UncheckedCharacterCodingException(e);
+                        }
+                    } while (isAdditionalDecodingRequired);
+
+                    encodedBlock.compact();
+                    state.setIsNewLineCharacterHandlingEnabled(isNewLineCharacterHandlingEnabled);
+
+                    return state;
+                }))
+                .flatMapIterable(state -> state.getLines())
+                .then(lineReaderState.map(state -> {
+                    final CharsetDecoder charsetDecoder = state.getCharsetDecoder();
+                    final CharBuffer finalDecodedBlock = state.getDecodedBlock();
+                    final ByteBuffer finalEncodedBlock = state.getEncodedBlock();
+                    final List<String> lines = state.getLines();
+                    final StringBuilder stringBuilder = state.getStringBuilder();
+
+                    finalEncodedBlock.flip();
+                    charsetDecoder.decode(finalEncodedBlock, finalDecodedBlock, true); // TODO: Handle any potential errors.
+                    charsetDecoder.flush(finalDecodedBlock); // TODO: Handle any potential errors.
+                    finalDecodedBlock.flip();
+
+                    final int l = finalDecodedBlock.limit();
+                    final int p = finalDecodedBlock.position();
+
+                    stringBuilder.append(finalDecodedBlock.array(), p, (l - p));
+                    lines.add(stringBuilder.toString());
+                    stringBuilder.setLength(0);
+                    finalDecodedBlock.position(l);
+
+                    return state;
+                }))
+                .flatMapIterable(state -> state.getLines());
+        };
+    }
+    private static int nthIndexOf(final String value, final char charToFind, final int n) {
         int count = 0;
         int index = -1;
 
         do {
-            index = s.indexOf(c, ++index);
+            index = value.indexOf(charToFind, ++index);
 
             if (-1 == index) {
                 break;
@@ -71,6 +161,51 @@ public class EventGridMonitor {
         } while (++count < n);
 
         return index;
+    }
+    private static List<String> split(final char delimitChar, final char quoteChar, final String lineValue) {
+        if (null != lineValue) {
+            final int lineLength = lineValue.length();
+            final List<String> recordValue = new ArrayList<>();
+
+            int fieldHead = 0;
+            int fieldTail = 0;
+            String fieldValue = "";
+            boolean isQuoted = false;
+
+            while (fieldTail < lineLength) {
+                char currentChar = lineValue.charAt(fieldTail++);
+
+                if (currentChar == quoteChar) {
+                    fieldValue = appendFieldChunk(fieldValue, lineValue, fieldHead, (fieldTail - fieldHead - 1));
+
+                    if ((fieldTail < lineLength) && (lineValue.charAt(fieldTail) == quoteChar)) {
+                        fieldValue += quoteChar;
+                        fieldTail++;
+                    }
+                    else {
+                        isQuoted = !isQuoted;
+                    }
+
+                    fieldHead = fieldTail;
+                }
+
+                if (!isQuoted && (currentChar == delimitChar)) {
+                    fieldValue = appendFieldChunk(fieldValue, lineValue, fieldHead, (fieldTail - fieldHead - 1));
+                    appendFieldValue(recordValue, fieldValue, quoteChar);
+
+                    fieldHead = fieldTail;
+                    fieldValue = "";
+                }
+            }
+
+            fieldValue = appendFieldChunk(fieldValue, lineValue, fieldHead, (fieldTail - fieldHead));
+            appendFieldValue(recordValue, fieldValue, quoteChar);
+
+            return recordValue;
+        }
+        else {
+            return null;
+        }
     }
 
     @FunctionName("EventGridMonitor")
@@ -104,7 +239,9 @@ public class EventGridMonitor {
         try {
             final Disposable processBlobOperation = blobClient
                 .downloadStream()
-                .transform(createLineReader(16384, Utf8Charset, logger::info))
+                .transform(createLineReader(16384, Utf8Charset))
+                .map(line -> split(',', '"', line))
+                .doOnNext(lines -> logger.info(lines.get(0)))
                 .doFinally(signalType -> {
                     logger.info("Processed blob: " + event.subject);
                 })
--- src/main/java/com/byteterrace/azure/functions/LineReaderState.java
+++ src/main/java/com/byteterrace/azure/functions/LineReaderState.java
@@ -2,91 +2,16 @@ package com.byteterrace.azure.functions;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
-import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CoderResult;
 import java.nio.charset.CodingErrorAction;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
+import java.util.ArrayList;
 
 public final class LineReaderState {
-    public static BiFunction<LineReaderState, ByteBuffer, LineReaderState> createReducer() {
-        return (state, input) -> {
-            final CharsetDecoder charsetDecoder = state.getCharsetDecoder();
-            final CharBuffer decodedBlock = state.getDecodedBlock();
-            final ByteBuffer encodedBlock = state.getEncodedBlock();
-            final Consumer<String> lineConsumer = state.getLineConsumer();
-            final StringBuilder stringBuilder = state.getStringBuilder();
-
-            boolean isAdditionalDecodingRequired = false;
-            boolean isNewLineCharacterHandlingEnabled = state.getIsNewLineCharacterHandlingEnabled();
-
-            encodedBlock.put(input);
-            encodedBlock.flip();
-
-            do {
-                try {
-                    final CoderResult decodeResult = charsetDecoder.decode(encodedBlock, decodedBlock, false);
-
-                    isAdditionalDecodingRequired = CoderResult.OVERFLOW.equals(decodeResult);
-
-                    if (CoderResult.UNDERFLOW.equals(decodeResult) || isAdditionalDecodingRequired) {
-                        decodedBlock.flip();
-
-                        final char[] a = decodedBlock.array();
-                        final int l = decodedBlock.limit();
-
-                        int o = 0;
-                        int p = decodedBlock.position();
-
-                        if (o < l) {
-                            do {
-                                final int c = a[o++];
-
-                                if ('\r' != c) {
-                                    if (('\n' == c) && isNewLineCharacterHandlingEnabled) {
-                                        stringBuilder.append(a, p, ((o - p) - 1));
-                                        lineConsumer.accept(stringBuilder.toString());
-                                        stringBuilder.setLength(0);
-                                        p = o;
-                                    }
-
-                                    isNewLineCharacterHandlingEnabled = true;
-                                }
-                                else {
-                                    stringBuilder.append(a, p, ((o - p) - 1));
-                                    lineConsumer.accept(stringBuilder.toString());
-                                    stringBuilder.setLength(0);
-                                    p = o;
-                                    isNewLineCharacterHandlingEnabled = false;
-                                }
-                            } while (o < l);
-                        }
-
-                        stringBuilder.append(a, p, (l - p));
-                        decodedBlock.clear();
-                    }
-                    else {
-                        decodeResult.throwException();
-                    }
-                }
-                catch (final CharacterCodingException e) {
-                    throw new UncheckedCharacterCodingException(e);
-                }
-            } while (isAdditionalDecodingRequired);
-
-            encodedBlock.compact();
-            state.setIsNewLineCharacterHandlingEnabled(isNewLineCharacterHandlingEnabled);
-
-            return state;
-        };
-    }
-
     private final CharsetDecoder m_charsetDecoder;
     private final CharBuffer m_decodedBlock;
     private final ByteBuffer m_encodedBlock;
-    private final Consumer<String> m_lineConsumer;
+    private final ArrayList<String> m_lines;
     private final StringBuilder m_stringBuilder;
 
     private boolean m_isNewLineCharacterHandlingEnabled;
@@ -106,14 +31,14 @@ public final class LineReaderState {
     public void setIsNewLineCharacterHandlingEnabled(boolean value) {
         m_isNewLineCharacterHandlingEnabled = value;
     }
-    public Consumer<String> getLineConsumer() {
-        return m_lineConsumer;
+    public ArrayList<String> getLines() {
+        return m_lines;
     }
     public StringBuilder getStringBuilder() {
         return m_stringBuilder;
     }
 
-    public LineReaderState(int bufferSize, Charset charset, Consumer<String> lineConsumer) {
+    public LineReaderState(int bufferSize, Charset charset) {
         final CharsetDecoder charsetDecoder = charset
             .newDecoder()
             .onMalformedInput(CodingErrorAction.REPORT)
@@ -122,7 +47,7 @@ public final class LineReaderState {
         m_charsetDecoder = charsetDecoder;
         m_decodedBlock = CharBuffer.allocate((int)(((double)bufferSize) * charsetDecoder.maxCharsPerByte()));;
         m_encodedBlock = ByteBuffer.allocate(bufferSize);
-        m_lineConsumer = lineConsumer;
+        m_lines = new ArrayList<>();
         m_stringBuilder = new StringBuilder(137);
 
         setIsNewLineCharacterHandlingEnabled(false);
